9a050e16f1b082440d2ca71f63b125c5c53bf36b
Jorge Cárcamo Caucamán <85809041+kuhaku001@users.noreply.github.com>
2021-12-15 18:22:29 -0300

    terminado CRUD.py

--- Modulos/CRUD.py
+++ Modulos/CRUD.py
@@ -11,11 +11,56 @@ class MySql:
         self.cursor = self.connection.cursor()
         print("Conexion excitosa")
 
-    def seleccionarTodo(self):
-        pass
-#por modificar
-    def ingresar(self, id, nombre, edad):
-        sql = "insert into empledos (id_empledo,Nombre_Completo,rut) values ({}, '{}', {})".format(id,nombre,edad)
+
+    def seleccionarTodo(self, nombre_tabla):
+        sql = 'select * from {}'.format(nombre_tabla)
+        try:
+            self.cursor.execute(sql)
+            rows = self.cursor.fetchall()
+            for row in rows:
+                for j in row:
+                    print(j, end=' ')
+                print()
+
+        except Exception as e:
+            print('error')
+
+    def ingresar(self, Nombre_Tabla , dato1 = '', dato2 = '', dato3 = '', dato4 = '', dato5 = '', dato6 = '', dato7 = '', dato8 = ''):
+        try:
+            if(Nombre_Tabla == 'empleado' ):
+                sql = "insert intro empleado (id_empleado, rut, nombreCompleto, fechadenaciminto, fotoCarnet, contraseña, nombreusurio, puesto) values ({},{},{},{},{},{},{},{})".format(dato1, dato2, dato3, dato4, dato5, dato6, dato7, dato8)
+                self.cursor.execute(sql)
+                self.connection.commit()
+            elif(Nombre_Tabla == 'bodega'):
+                sql = "insert intro bodega (id_bodega, Capacidad, Direccion, Jefe_Bodega) values ({}, {}, '{}', '{}')".format(dato1, dato2, dato3, dato4)
+                self.cursor.execute(sql)
+                self.connection.commit()
+            elif(Nombre_Tabla == 'productos'):
+                sql = "insert intro productos (id_productos, codigo_De_Barras, Nombre_Producto, Categoria, Marca, Precio_Unitario, Cantidad_Total, Stock_Critico) values ({},'{}','{}','{}','{}',{},{},{})".format(dato1, dato2, dato3, dato4, dato5, dato6, dato7, dato8)
+                self.cursor.execute(sql)
+                self.cursor.execute(sql)
+                self.connection.commit()
+            elif(Nombre_Tabla == 'ordenes'):
+                sql = "insert intro ordenes (id_ordenes, Folio, Productos, Cantidad_Por_Producto, Precio_Total) values ({},'{}', '{}', {}, {})".format(dato1, dato2, dato3, dato4, dato5)
+                self.cursor.execute(sql)
+                self.connection.commit()
+            elif(Nombre_Tabla == 'cliente'):
+                sql = "insert into cliente (id_cliente, Nombre, Rut, Direccion, telefono, Correo) values ({}, '{}', '{}', '{}', '{}', '{}')".format(dato1, dato2, dato3, dato4, dato5, dato6)
+                self.cursor.execute(sql)
+                self.connection.commit()
+        except Exception as e:
+            print("El valor ya existe", e)
+
+    def actualizar(self,id,puesto):
+        sql = "update empleado set puesto='{}' where id_empleado={}".format(puesto,id)
+        try:
+            self.cursor.execute(sql)
+            self.connection.commit()
+        except Exception as e:
+            print('error')
+
+    def borrar(self, Nombre_Tabla = str, id = int):
+        sql = "delete from {} where id = {}".format(Nombre_Tabla, id)
         try:
             self.cursor.execute(sql)
             self.connection.commit()
@@ -23,13 +68,9 @@ class MySql:
             print("El valor ya existe",e)
             raise
 
-    def borrar(self, Nombre_Bd = str, id = int):
-        sql = "delete from {} where id = {}".format(Nombre_Bd, id)
-        try:
-            self.cursor.execute(sql)
-            self.connection.commit()
-        except Exception as e:
-            print("El valor ya existe",e)
-            raise
+      
+    def cerrar(self):
+        self.connection.close()
+        print('Conexion cerrada')
 
-Crud = MySql()
+  
