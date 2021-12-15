3e0433826b87dbda0ea6d0a584ec84732e130920
Jorge Cárcamo Caucamán <85809041+kuhaku001@users.noreply.github.com>
2021-12-14 23:01:30 -0300

    Guardado Cliente

---
+++ Modulos/Cliente.py
@@ -0,0 +1,7 @@
+class Cliente:
+    def __init__(self, nombre, Rut, Direccion, TelefonosDeContacto, CorreoElectronico):
+        self.nombre = nombre
+        self.Rut = Rut
+        self.Direccion = Direccion
+        self.Telefono = TelefonosDeContacto
+        self.Correo = CorreoElectronico
